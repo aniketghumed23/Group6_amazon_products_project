0621e0adad7f3250c667f3df4eb974229a5f819a
aniketghumed23 <112738810+aniketghumed23@users.noreply.github.com>
2023-12-07 20:51:06 -0500

    phase3 notebook uploaded

---
+++ phase3_notebook/train.py
@@ -0,0 +1,43 @@
+# train.py
+import subprocess
+import sys
+
+# Install fsspec package
+subprocess.check_call([sys.executable, "-m", "pip", "install", "fsspec"])
+
+import argparse
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+import joblib
+
+if __name__ == '__main__':
+    # Parse arguments passed to the script
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    args = parser.parse_args()
+
+    # Load data from the specified directory
+    data = pd.read_csv(os.path.join(args.train, 'training_dataset.csv'))
+    
+    # Select relevant features and target variable
+    X = data[['reviews', 'price']]
+    y = data['isBestSeller']
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Create a logistic regression model
+    model = LogisticRegression()
+
+    # Train the model on the training data
+    model.fit(X_train, y_train)
+
+    # Evaluate the model
+    predictions = model.predict(X_test)
+    print(classification_report(y_test, predictions))
+
+    # Save the trained model
+    joblib.dump(model, os.path.join(os.environ['SM_MODEL_DIR'], 'model.joblib'))
